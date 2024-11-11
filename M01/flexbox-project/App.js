10d0977f955dbcc780331fa6399950c604593da5
WyattHPearce <wpearce447@gmail.com>
2024-11-11 16:50:59 -0500

    Flexbox assignment continued work

--- M01/flexbox-project/App.js
+++ M01/flexbox-project/App.js
@@ -4,10 +4,10 @@ import styles from "./styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.box}>
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.box}>
         <Text style={styles.boxText}>I'm in a box</Text>
-      </View>
-    </View>
+      </SafeAreaView>
+    </SafeAreaView>
   );
 }
