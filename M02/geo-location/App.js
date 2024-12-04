9c30b7be2104d8195f4f50f2e4f0bd3623c92548
WyattHPearce <wpearce447@gmail.com>
2024-12-04 14:29:11 -0500

    Updating geo-location

--- M02/geo-location/App.js
+++ M02/geo-location/App.js
@@ -1,7 +1,55 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// External Imports
+import React, { useState, useEffect } from "react";
+import { Text, SafeAreaView } from "react-native";
+import * as Location from "expo-location";
+import axios from "axios";
+// Local Imports
+import styles from "./styles";
+
+const API_URL = "https://nominatim.openstreetmap.org/reverse";
+
+// Function to format the address
+function formatAddress(address) {
+  // Extract individual elements
+  const {
+    shop,
+    house_number,
+    road,
+    neighborhood,
+    suburb,
+    county,
+    city,
+    state,
+    iso,
+    postcode,
+    country,
+    countryCode,
+  } = address;
+
+  // Format a string from extracted data
+  return [
+    house_number,
+    road,
+    neighborhood,
+    suburb,
+    city,
+    state,
+    postcode,
+    country,
+  ]
+    .filter(Boolean)
+    .join(", ");
+}
 
 export default function App() {
+  // Program states
+  const [address, setAddress] = useState("Loading...");
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
+
+  // Getting the users location from device
+  useEffect(() => {});
+
   return (
     <View style={styles.container}>
       <Text>Geo-Location basic setup complete.</Text>
@@ -9,12 +57,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
